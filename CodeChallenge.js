c45252b3164f4fe4190d28128d0ef73d6690e59e
Lucas Saucer <saucerL05@my.sunyulster.edu>
2024-01-15 22:17:38 -0500

    Finish Logic, Edit CSS
    Finish JavaScript logic, begin editing CSS in webpage for final submission.

--- CodeChallenge.js
+++ CodeChallenge.js
@@ -1,277 +1,293 @@
 // Code for  Kaizen UX Code Challenge by Luke Saucer. CSS file is attached but is put into the variable customStyles below so that you can insert the JavaScript file into the Chrome Developer Tools console and it will work.
 
-var link = document.createElement('link'); 
-link.rel = 'stylesheet';
-link.href = 'CodeChallenge.css';
-document.head.appendChild(link); 
-
-
-
+// var link = document.createElement('link'); 
+// link.rel = 'stylesheet';
+// link.href = 'CodeChallenge.css';
+// document.head.appendChild(link); 
 
 //CSS rules
-// var customStyles = `
-// .container-cc {
-//     height: auto;
-//     width: 100%;
-//     display: flex;
-//     justify-content: center;
-//     background-color: darkgray;
-//   }
+var customStyles = `
+.container-cc {
+    height: auto;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: darkgray;
+  }
 
-// .box-cc {
-//     height: 250px;
-//     max-width: 1270px;
-//     justify-content: center;
-//     border-radius: 10px;
-//     background-color: white;
-//     margin: 2em;
-//   }
+.box-cc {
+    height: 250px;
+    max-width: 1270px;
+    justify-content: center;
+    border-radius: 10px;
+    background-color: white;
+    margin: 2em;
+  }
 
-// .container-cc .box-cc {
-//     display: flex;
-//     align-items: center;
-// }
+.container-cc .box-cc {
+    display: flex;
+    align-items: center;
+}
 
-// .container-cc .box-cc .left-cc {
-//     height: auto;
-//     display: flex;
-//     flex-direction: column;
-//     flex-basis: 20%; 
-//     align-items: flex-start;
-// }
+.container-cc .box-cc .left-cc {
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    flex-basis: 20%; 
+    align-items: flex-start;
+}
 
-// .container-cc .box-cc .middle-cc {
-//     height: auto;
-//     display: flex;
-//     flex-direction: column;
-//     flex-basis: 40%; 
-// }
+.container-cc .box-cc .middle-cc {
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    flex-basis: 40%; 
+}
 
-// .container-cc .box-cc .right-cc {
-//     height: auto;
-//     display: flex;
-//     flex-direction: column; 
-//     flex-basis: 40%;
-// }
+.container-cc .box-cc .right-cc {
+    height: auto;
+    display: flex;
+    flex-direction: column; 
+    flex-basis: 40%;
+}
 
-// .container-cc .box-cc .left-cc,
-// .container-cc .box-cc .middle-cc,
-// .container-cc .box-cc .right-cc {
-//     background-color: white;
-//     border-radius: 10px; 
-//     padding: .5em;
-//     display: flex;
-//     justify-content: center;
-// }
+.container-cc .box-cc .left-cc,
+.container-cc .box-cc .middle-cc,
+.container-cc .box-cc .right-cc {
+    background-color: white;
+    border-radius: 10px; 
+    padding: .5em;
+    display: flex;
+    justify-content: center;
+}
 
-// .container-cc .box-cc .middle-cc h1 {
-//     font-size: 1.1em;
-//     font-family: 'Roboto', sans-serif;
-//     font-weight: bold;
-//     margin-left: .9em;
-// }
+.container-cc .box-cc .middle-cc h1 {
+    font-size: 1.1em;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    margin-left: .9em;
+}
 
-// .container-cc .box-cc .middle-cc p {
-//     margin-top: 0.5em;
-//     font-size: 1em;
-//     font-family: 'Roboto', sans-serif;
-// }
-// .container-cc .box-cc .left-cc button {
-//     font-size: .8em;
-//     font-family: 'Roboto', sans-serif;
-// }
+.container-cc .box-cc .middle-cc p {
+    margin-top: 0.5em;
+    font-size: 1em;
+    font-family: 'Roboto', sans-serif;
+}
+.container-cc .box-cc .left-cc button {
+    font-size: .8em;
+    font-family: 'Roboto', sans-serif;
+}
 
-// .container-cc .box-cc .left-cc .button-container-cc {
-//     display: flex;
-//     align-items: flex-start;
-// }
+.container-cc .box-cc .left-cc .button-container-cc {
+    display: flex;
+    align-items: flex-start;
+}
 
-// .btn-primary-cc {
-//   margin-top: 1em;
-//   border: 1px solid transparent;
-//   border-radius: 40px;
-//   background-color:darkred;
-//   color: white;
-//   font-weight: bold;
-// }
+.container-cc .box-cc .image-container-cc img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+    max-height: 100%;
+    height: auto;
+}
 
-// .btn-secondary-cc {
-//     margin-top: .8em;
-//     border: 1px solid transparent;
-//     border-radius: 40px;
-//     background-color: white;
-//     color: darkred;
-//     font-weight: bold;
+.btn-primary-cc {
+  margin-top: 1em;
+  border: 1px solid transparent;
+  border-radius: 40px;
+  background-color:darkred;
+  color: white;
+  font-weight: bold;
+}
 
-// }
+.btn-secondary-cc {
+    margin-top: .8em;
+    border: 1px solid transparent;
+    border-radius: 40px;
+    background-color: white;
+    color: darkred;
+    font-weight: bold;
 
-// .btn-primary-cc:hover, .btn-secondary-cc:hover {
-//     /* Styles for btn-primary-cc when it's hovered */
-//     background-color: firebrick;
-//     color: white;
-// }
+}
 
-// .btn-primary-cc.active, .btn-secondary-cc.active {
-//     /* Styles for btn-primary-cc when another button is active */
-//     background-color: darkred;
-//     color: white;
-// }
+.btn-tertiary-cc {
+    margin-top: .8em;
+    border: 1px solid transparent;
+    border-radius: 40px;
+    background-color: white;
+    color: darkred;
+    font-weight: bold;
+}
 
-// .btn-primary-cc.inactive, .btn-secondary-cc.inactive {
-//     /* Styles for btn-primary-cc when it's inactive */
-//     background-color: white;
-//     color: darkred;
-// }
+.btn-primary-cc:hover, .btn-secondary-cc:hover, .btn-tertiary-cc:hover {
+    /* Styles for btn-primary-cc when it's hovered */
+    background-color: firebrick;
+    color: white;
+}
 
-// .btn-primary-cc.clicked, .btn-secondary-cc.clicked {
-//     /* Styles for buttons that have been clicked */
-//     background-color: darkred;
-//     color: white;
-// }
+.btn-primary-cc.active, .btn-secondary-cc.active, .btn-tertiary-cc.active {
+    /* Styles for btn-primary-cc when another button is active */
+    background-color: darkred;
+    color: white;
+}
 
-// #CodeChallengeImage {
-//     width: 100%;
-//     height: 100%;
-//     border-radius: 10px;
-// }
+.btn-primary-cc.inactive, .btn-secondary-cc.inactive, .btn-tertiary-cc.inactive {
+    /* Styles for btn-primary-cc when it's inactive */
+    background-color: white;
+    color: darkred;
+}
 
-// .image-container-cc {
-//     display: flex;
-//     height: 60%;
-//     width: 50%;
-//     margin: 10px 0px 10px 80px;
-// }
+.btn-primary-cc.clicked, .btn-secondary-cc.clicked, .btn-tertiary-cc.clicked {
+    /* Styles for buttons that have been clicked */
+    background-color: darkred;
+    color: white;
+}
 
-// /* Styles for screens larger than 561px */
-// @media (min-width: 561px) {
-//     .container-cc {
-//         margin: auto;
-//     }
+#CodeChallengeImage {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+}
 
-//     .box-cc {
-//         display: flex;
-//     }
+.image-container-cc {
+    display: flex;
+    height: 60%;
+    width: 50%;
+    margin: 10px 0px 10px 80px;
+}
 
-//     .left-cc, .middle-cc, .right-cc {
-//         flex: 1;
-//     }
+/* Styles for screens larger than 561px */
+@media (min-width: 561px) {
+    .container-cc {
+        margin: auto;
+    }
 
-//     .container-cc .box-cc .middle-cc h1 {
-//         margin-left: 3.6em;
-//     }
+    .box-cc {
+        display: flex;
+    }
 
-//     .container-cc .box-cc .middle-cc p {
-//     margin-top: 0.5em;
-//     font-size: 1em;
-//     font-family: 'Roboto', sans-serif;
-//     margin-left: 4em;
-//     }
+    .left-cc, .middle-cc, .right-cc {
+        flex: 1;
+    }
+
+    .container-cc .box-cc .middle-cc h1 {
+        margin-left: 3.6em;
+    }
+
+    .container-cc .box-cc .middle-cc p {
+    margin-top: 0.5em;
+    font-size: 1em;
+    font-family: 'Roboto', sans-serif;
+    margin-left: 4em;
+    }
     
-//     .button-container-cc {
-//         display: flex;
-//         flex-direction: column;
-//         box-shadow: none; /* This removes the shadowed box-cc */
-//     }
+    .button-container-cc {
+        display: flex;
+        flex-direction: column;
+        box-shadow: none; /* This removes the shadowed box-cc */
+    }
 
-//     .container-cc .box-cc .left-cc .button-container-cc {
-//         margin-left: 1.5em;
-//         margin-bottom: 1.5em;
-//     }
+    .container-cc .box-cc .left-cc .button-container-cc {
+        margin-left: 1.5em;
+        margin-bottom: 1.5em;
+    }
 
-//     .container-cc .box-cc .left-cc button {
-//         font-size: .8em;
-//         padding: .5em 1.3em;
-//         font-family: 'Roboto', sans-serif;
-//     }
-// }
+    .container-cc .box-cc .left-cc button {
+        font-size: .8em;
+        padding: .5em 1.3em;
+        font-family: 'Roboto', sans-serif;
+    }
+}
 
-// /* Styles for screens smaller than or equal to 560px */
-// @media (max-width: 560px) {
-//     .container-cc {
-//         width: 100%;
-//     }
+/* Styles for screens smaller than or equal to 560px */
+@media (max-width: 560px) {
+    .container-cc {
+        width: 100%;
+    }
 
-//     .container-cc .box-cc .right-cc .img {
-//         flex-direction: column;
-//     }
+    .container-cc .box-cc .right-cc .img {
+        flex-direction: column;
+    }
 
-//     .container-cc .box-cc {
-//         flex-direction: column;
-//         height: 100%;
-//         margin-top: 2.3em;
-//         margin-bottom: 2.3em;
-//         margin-left: .5em;
-//         margin-right: .5em;
-//         width: 90%;
-//         padding-bottom: 1em;
-//     }
+    .container-cc .box-cc {
+        flex-direction: column;
+        height: 100%;
+        margin-top: 2.3em;
+        margin-bottom: 2.3em;
+        margin-left: .5em;
+        margin-right: .5em;
+        width: 90%;
+        padding-bottom: 1em;
+    }
 
-//     .box-cc {
-//         display: flex;
-//     }
+    .box-cc {
+        display: flex;
+    }
 
-//     .left-cc, .middle-cc, .right-cc {
-//         width:95%;
-//     }
+    .left-cc, .middle-cc, .right-cc {
+        width:95%;
+    }
 
-//    .container-cc .box-cc .middle-cc h1 {
-//         margin-left: 1.4em;
-//         margin-top: .75em;
-//         font-size: large;
-//     }
+   .container-cc .box-cc .middle-cc h1 {
+        margin-left: 1.5em;
+        margin-top: .75em;
+        font-size: large;
+    }
 
-//     .container-cc .box-cc .middle-cc p {
-//         margin-left: 1.8em;
-//         margin-right: 1.8em;
-//     }
+    .container-cc .box-cc .middle-cc p {
+        margin-left: 1.8em;
+        margin-right: 1.8em;
+    }
 
-//     .button-container-cc {
-//         justify-content: space-evenly;
-//         box-shadow: 0px 4px 3px 0px rgba(0, 0, 0, 0.15); /* This adds a shadowed box-cc */
-//         border: 1px solid transparent;
-//         border-radius: 2.5px;
-//         width: 90%;
-//         margin-bottom: .5em;
-//         margin-top: 1.2em;
-//     }
+    .button-container-cc {
+        justify-content: space-evenly;
+        box-shadow: 0px 4px 3px 0px rgba(0, 0, 0, 0.15); /* This adds a shadowed box-cc */
+        border: 1px solid transparent;
+        border-radius: 2.5px;
+        width: 99%;
+        margin-bottom: .5em;
+        margin-top: 1.2em;
+        padding-left: .2em;
+        padding-right: .2em;
+    }
 
-//     .button-container-cc button {
-//         margin: 10px 10px; /* This adds 10px of space to the left-cc and right-cc of each button */
-//     }
+    .button-container-cc button {
+        margin: 10px 10px; /* This adds 10px of space to the left-cc and right-cc of each button */
+    }
 
-//     .container-cc .box-cc .left-cc {
-//         align-items: center;
-//         justify-content: center;
-//     }
+    .container-cc .box-cc .left-cc {
+        align-items: center;
+        justify-content: center;
+    }
 
-//     .image-container-cc {
-//         width: 80%;
-//         margin: 2em auto;
-//     }
+    .image-container-cc {
+        width: 80%;
+        margin: 2em auto;
+    }
     
-//     .image-container-cc img {
-//         display: flex;
-//         margin: auto;
-//     }
+    .image-container-cc img {
+        display: flex;
+        margin: auto;
+    }
 
-//     .container-cc .box-cc .left-cc button {
-//         font-size: 1em;
-//         padding: .6em 1.3em;
-//         font-family: 'Roboto', sans-serif;
-//     }
+    .container-cc .box-cc .left-cc button {
+        font-size: 1em;
+        padding: .6em 1.3em;
+        font-family: 'Roboto', sans-serif;
+    }
 
-// }
-// `;
+}
+`;
 
 // Create a style element
-// var styleElement = document.createElement('style');
+var styleElement = document.createElement('style');
 
 // Set the styles
-// styleElement.innerHTML = customStyles;
+styleElement.innerHTML = customStyles;
 
 // Append the style element to the head
-// document.head.appendChild(styleElement);
-
+document.head.appendChild(styleElement);
 
 var newSectionHTML = '' +
 '<div class="container-cc">' + 
@@ -298,33 +314,32 @@ var newSectionHTML = '' +
 
 // Code for Chrome Developer Tools Console
 // Finding the target element with the class "schedule"
-// var targetElement = document.querySelector('.schedule');
+var targetElement = document.querySelector('.schedule');
 
 // Inserting the HTML above the "schedule" section
-// if (targetElement) {
-//   targetElement.insertAdjacentHTML('beforebegin', newSectionHTML);
-// } else {
-//   console.error('Element with class "schedule" not found.');
-// }
-
+if (targetElement) {
+  targetElement.insertAdjacentHTML('beforebegin', newSectionHTML);
+} else {
+  console.error('Element with class "schedule" not found.');
+}
 
 // Code for VSCode
-var newDiv = document.createElement("div"); 
+// var newDiv = document.createElement("div"); 
 // Set the div's innerHTML to newSectionHTML
-newDiv.innerHTML = newSectionHTML;
+// newDiv.innerHTML = newSectionHTML;
 // Append the new div to the body
-document.body.appendChild(newDiv);
+// document.body.appendChild(newDiv);
 
-
-
-
-
-// Now that the new HTML is added to the page, get the buttons, header, text, and image elements
+// Get the buttons, header, text, and image elements etc.
 var buttons = document.querySelectorAll('.container-cc .box-cc .left-cc button');
 var header = document.querySelector('.container-cc .box-cc .middle-cc h1');
 var text = document.querySelector('.container-cc .box-cc .middle-cc p');
 var image = document.querySelector('.container-cc .box-cc .right-cc img');
 var btnPrimary = document.querySelector('.btn-primary-cc');
+var btnSecondary = document.querySelector('.btn-secondary-cc');
+var btnTertiary = document.querySelector('.btn-tertiary-cc');
+var imageContainer = document.querySelector('.image-container-cc');
+// Array holding the matches on the schedule.
 var matches = [
     {
       team: 'Presbyterian',
@@ -514,7 +529,9 @@ var matches = [
     },
   ];
 
+// Get a random match from the matches array
 var matchIndex = Math.floor(Math.random() * matches.length);
+// Get the match at the random index
 var match = matches[matchIndex];
 
 // Define the headers, text, and image URLs for each button
@@ -525,22 +542,6 @@ var texts = [
     'Vanderbilt Basketball\'s next matchup is against ' + match.team + ' at ' + match.time + ' on ' + match.date + '.'
 ];
 var images = ['https://vucommodores.com/wp-content/uploads/2024/01/Screenshot-2024-01-11-at-3.03.33-PM-1024x530.png', 'https://vucommodores.com/wp-content/uploads/2024/01/DSC05578-1-1024x683.jpg'];
-
-// //Create img element for the team's image.
-// var img = document.createElement('img');
-// img.src = match.image;
-
-// //Select the image-container-cc element
-// var imageContainer = document.querySelector('.image-container-cc');
-
-// Remove any existing images in the container
-// while(imageContainer.firstChild) {
-//     imageContainer.removeChild(imageContainer.firstChild);
-// }
-
-// //Append the image to the image-container-cc element
-// imageContainer.appendChild(img);
-
 
 // Loop through the buttons
 buttons.forEach(function(button, index) {
@@ -553,6 +554,8 @@ btnPrimary.addEventListener('mouseenter', function() {
 btnPrimary.addEventListener('mouseleave', function() {
     // Only add the inactive class back if the btn-secondary-cc button is clicked
     if (document.querySelector('.btn-secondary-cc').classList.contains('clicked')) {
+        this.classList.add('inactive');
+    } else if (document.querySelector('.btn-tertiary-cc').classList.contains('clicked')) {
         this.classList.add('inactive');
     }
 });
@@ -567,26 +570,19 @@ btnPrimary.addEventListener('mouseleave', function() {
         // Add the active class to the clicked button
         button.classList.add('clicked');
 
-            // Add the inactive class to the primary button if the secondary button is clicked
-        if (button.classList.contains('btn-secondary-cc')) {
-            document.querySelector('.btn-primary-cc').classList.add('inactive');
-        } else {
-            document.querySelector('.btn-primary-cc').classList.remove('inactive');
-        } 
-
+// Add the inactive class to the primary button if the secondary or tertiary button is clicked
+if (button.classList.contains('btn-secondary-cc') || button.classList.contains('btn-tertiary-cc')) {
+    document.querySelector('.btn-primary-cc').classList.add('inactive');
+} else {
+    document.querySelector('.btn-primary-cc').classList.remove('inactive');
+}
         header.textContent = headers[index];
         text.innerHTML = texts[index]; // Change textContent to innerHTML
-
         // Change the image in the right-cc column
-        image.src = images[index];
-
-
-     // Create an img element
-     var img = document.createElement('img');
-
-     // Select the image-container-cc element
-     var imageContainer = document.querySelector('.image-container-cc');
- 
-     
+        if (this.classList.contains('btn-tertiary-cc')) {
+            image.src = match.image; // Assuming match is a globally accessible object
+        } else {
+            image.src = images[index];
+        }
     });
 });
